475c6f30dc4dce2672532280a8290a830447bbf7
Cgalvispadilla <fktcg99@gmail.com>
2021-09-09 22:31:11 -0500

    Se creo la crearon los servicios para la persona y se implemeto la interfaz que tenía las funcionalidades del crud

---
+++ src/main/java/com/sofkau/persona/services/PersonService.java
@@ -0,0 +1,46 @@
+package com.sofkau.persona.services;
+
+import com.sofkau.persona.entities.Person;
+import com.sofkau.persona.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService implements InterfaceServicesPerson {
+
+    @Autowired
+    private PersonRepository data;
+
+    @Override
+    public Person save(Person persona) {
+        return data.save(persona);
+    }
+
+    @Override
+    public List<Person> list() {
+        return data.findAll();
+    }
+
+    @Override
+    public Person listById(int id) {
+        return data.getById(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        data.deleteById(id);
+    }
+
+    @Override
+    public Person update(Person persona, int id) {
+        return  data.findById(id)
+                .map(person -> {
+                    person.setId(persona.getId());
+                    person.setNombre(persona.getNombre());
+                    person.setEdad(persona.getEdad());
+                    return data.save(person);
+                }).get();
+    }
+}
